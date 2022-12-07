138aba4536adfd2d92c48224adbb0adb347883e4
ainas <puidokasainas@gmail.com>
2022-12-07 20:34:14 +0200

    completed CAOnline task

--- pamoka23/uzduotis2.js
+++ pamoka23/uzduotis2.js
@@ -1,2 +1,18 @@
 // CodeAcademy Online Tasks
 // Task 1
+
+{
+    const name = "Ainas";
+    const amount = 10;
+    for (let i = 0; i < amount; i++) {
+        console.log(`${i}. ${name}`);
+    }
+}
+
+// Task 2
+
+{
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
