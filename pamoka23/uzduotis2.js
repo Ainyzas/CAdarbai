daf66f71d3c91bcccdeb53d2cd70fcab2e5ffc87
ainas <puidokasainas@gmail.com>
2022-12-07 21:36:22 +0200

    improved car brand checking task

--- pamoka23/uzduotis2.js
+++ pamoka23/uzduotis2.js
@@ -16,3 +16,15 @@
         console.log(i);
     }
 }
+
+{
+    let car = prompt("Enter your car manufacturer");
+
+    if (["VW", "Audi", "Bentley", "Bugatti", "Lambo", "Porsche"].includes(car)) {
+        console.log("Your car belongs to VW club");
+    } else if (["BMW", "Mini", "Rolls-Royce"].includes(car)) {
+        console.log("Your car belongs to BMW club");
+    } else {
+        console.log("Your car belongs nether club");
+    }
+}
