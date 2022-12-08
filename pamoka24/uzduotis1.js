97b8071f6a8480110bab1da524812f86514f2211
ainas <puidokasainas@gmail.com>
2022-12-08 22:29:47 +0200

    Completed all given tasks

---
+++ pamoka24/uzduotis1.js
@@ -0,0 +1,57 @@
+// Task 1
+{
+    const name = "John";
+    let i = 3;
+    while (i > 0) {
+        console.log(name);
+        i--;
+    }
+}
+
+// Task 2
+
+{
+    let combo = "";
+    const name = "John";
+    let times = 3;
+    do {
+        combo += name;
+        times--;
+    } while (times > 0);
+    console.log(combo);
+}
+
+// Task 3
+
+{
+    Math.cos(0);
+
+    Math.floor(Math.random() * 5) + 1;
+
+    Math.floor(Math.random() * (12 - 5 + 1)) + 5;
+
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    if (randomNumber === 1) {
+        alert("You won");
+    } else {
+        alert("Try again next time");
+    }
+}
+
+// Task 4
+
+{
+    const text = document.querySelector(".bluetext span");
+    text.textContent = "blue";
+    console.log(text.innerHTML);
+}
+
+// Task 5
+
+{
+    const firstList = document.querySelector("ol li:first-child");
+    const secondList = document.querySelector("ol li:nth-child(2)");
+    const temporary = firstList.textContent;
+    firstList.textContent = secondList.textContent;
+    secondList.textContent = temporary;
+}
