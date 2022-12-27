620378f073f0503025dec50f77fb43581d75fa84
ainas <puidokasainas@gmail.com>
2022-12-27 21:11:10 +0200

    Added task 2 of lesson 31

---
+++ pamoka31/uzduotis2.js
@@ -0,0 +1,7 @@
+//! Tips
+// collect elements to constants (e.g. startButton, secondsFiels, minutesField, ring)
+// add event listener on startButton and on click:
+// if it was START that was pressed, start interval with function, which will change values of minutes and seconds every interval;
+// if it was STOP that was pressed, stop interval;
+// ...
+// Good luck...
