3eca4162855e73791748704b00454a74af0cbeb1
ainas <puidokasainas@gmail.com>
2023-01-19 20:39:43 +0200

    commented js tasks

--- pamoka40/uzduotis3-1.js
+++ pamoka40/uzduotis3-1.js
@@ -1,14 +1,14 @@
-fetch("https://olive-bead-glazer.glitch.me")
-    .then(response => response.json())
-    .then(data => {
-        const table = document.querySelector("table");
+// fetch("https://olive-bead-glazer.glitch.me")
+//     .then(response => response.json())
+//     .then(data => {
+//         const table = document.querySelector("table");
 
-        let headers = "<tr><th>Brand</th><th>Model</th></tr>";
-        table.innerHTML = headers;
+//         let headers = "<tr><th>Brand</th><th>Model</th></tr>";
+//         table.innerHTML = headers;
 
-        data.forEach(car => {
-            let row = "<tr><td>" + car.brand + "</td><td>" + car.model + "</td></tr>";
-            table.innerHTML += row;
-        });
-    })
-    .catch(error => console.error(error));
+//         data.forEach(car => {
+//             let row = "<tr><td>" + car.brand + "</td><td>" + car.model + "</td></tr>";
+//             table.innerHTML += row;
+//         });
+//     })
+//     .catch(error => console.error(error));
