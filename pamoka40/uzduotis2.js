a7624a97b2b02633b6860bec3480c76473d5ed78
ainas <puidokasainas@gmail.com>
2023-01-23 18:11:40 +0200

    uncommented

--- pamoka40/uzduotis2.js
+++ pamoka40/uzduotis2.js
@@ -1,123 +1,123 @@
-// // Task 1
+// Task 1
 
-// {
-//     const body = document.querySelector("body");
-//     const url = "https://magnetic-melon-yam.glitch.me/";
-//     let currentData = {};
+{
+    const body = document.querySelector("body");
+    const url = "https://magnetic-melon-yam.glitch.me/";
+    let currentData = {};
 
-//     function createCheckbox() {
-//         const checkbox = document.createElement("input");
-//         checkbox.setAttribute("type", "checkbox");
-//         checkbox.setAttribute("id", "isVip");
-//         body.append(checkbox);
+    function createCheckbox() {
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("id", "isVip");
+        body.append(checkbox);
 
-//         const checkboxLabel = document.createElement("label");
-//         checkboxLabel.setAttribute("for", "isVip");
-//         checkboxLabel.innerText = "VIP";
-//         body.append(checkboxLabel);
-//     }
+        const checkboxLabel = document.createElement("label");
+        checkboxLabel.setAttribute("for", "isVip");
+        checkboxLabel.innerText = "VIP";
+        body.append(checkboxLabel);
+    }
 
-//     function createSearchForm() {
-//         const search = document.createElement("input");
-//         search.setAttribute("type", "search");
-//         search.setAttribute("id", "search");
-//         search.setAttribute("name", "search");
-//         body.appendChild(search);
+    function createSearchForm() {
+        const search = document.createElement("input");
+        search.setAttribute("type", "search");
+        search.setAttribute("id", "search");
+        search.setAttribute("name", "search");
+        body.appendChild(search);
 
-//         const searchButton = document.createElement("button");
-//         searchButton.innerHTML = "Search for name";
-//         searchButton.setAttribute("id", "searchButton");
+        const searchButton = document.createElement("button");
+        searchButton.innerHTML = "Search for name";
+        searchButton.setAttribute("id", "searchButton");
 
-//         const form = document.createElement("form");
-//         form.append(search, searchButton);
-//         body.append(form);
-//     }
+        const form = document.createElement("form");
+        form.append(search, searchButton);
+        body.append(form);
+    }
 
-//     function createTable() {
-//         const table = document.createElement("table");
-//         const header = document.createElement("tr");
-//         const tbody = document.createElement("tbody");
+    function createTable() {
+        const table = document.createElement("table");
+        const header = document.createElement("tr");
+        const tbody = document.createElement("tbody");
 
-//         const id = document.createElement("th");
-//         id.innerText = "ID";
+        const id = document.createElement("th");
+        id.innerText = "ID";
 
-//         const image = document.createElement("th");
-//         image.innerText = "Image";
+        const image = document.createElement("th");
+        image.innerText = "Image";
 
-//         const name = document.createElement("th");
-//         name.innerText = "Name";
+        const name = document.createElement("th");
+        name.innerText = "Name";
 
-//         const surname = document.createElement("th");
-//         surname.innerText = "Surname";
+        const surname = document.createElement("th");
+        surname.innerText = "Surname";
 
-//         const city = document.createElement("th");
-//         city.innerText = "City";
+        const city = document.createElement("th");
+        city.innerText = "City";
 
-//         const color = document.createElement("th");
-//         color.innerText = "Fav Color";
+        const color = document.createElement("th");
+        color.innerText = "Fav Color";
 
-//         header.append(id, image, name, surname, city, color);
-//         table.append(header, tbody);
-//         body.append(table);
-//     }
+        header.append(id, image, name, surname, city, color);
+        table.append(header, tbody);
+        body.append(table);
+    }
 
-//     function addData(robotData) {
-//         const tbody = document.querySelector("tbody");
-//         tbody.innerHTML = "";
+    function addData(robotData) {
+        const tbody = document.querySelector("tbody");
+        tbody.innerHTML = "";
 
-//         robotData.forEach(robot => {
-//             const id = document.createElement("td");
-//             id.innerText = robot.id;
+        robotData.forEach(robot => {
+            const id = document.createElement("td");
+            id.innerText = robot.id;
 
-//             const img = document.createElement("img");
-//             img.src = robot.image;
-//             img.setAttribute("alt", "UserPicture");
-//             const image = document.createElement("td");
-//             image.append(img);
+            const img = document.createElement("img");
+            img.src = robot.image;
+            img.setAttribute("alt", "UserPicture");
+            const image = document.createElement("td");
+            image.append(img);
 
-//             const [name, surname] = robot.name.split(" ");
+            const [name, surname] = robot.name.split(" ");
 
-//             const firstName = document.createElement("td");
-//             firstName.innerText = name;
+            const firstName = document.createElement("td");
+            firstName.innerText = name;
 
-//             const lastName = document.createElement("td");
-//             lastName.innerText = surname;
+            const lastName = document.createElement("td");
+            lastName.innerText = surname;
 
-//             const city = document.createElement("td");
-//             city.innerText = robot.city;
+            const city = document.createElement("td");
+            city.innerText = robot.city;
 
-//             const favColor = document.createElement("td");
-//             favColor.innerText = robot.fav_color;
+            const favColor = document.createElement("td");
+            favColor.innerText = robot.fav_color;
 
-//             const tr = document.createElement("tr");
-//             tr.append(id, image, firstName, lastName, city, favColor);
-//             tbody.append(tr);
-//         });
-//     }
+            const tr = document.createElement("tr");
+            tr.append(id, image, firstName, lastName, city, favColor);
+            tbody.append(tr);
+        });
+    }
 
-//     createCheckbox();
-//     createSearchForm();
-//     createTable();
+    createCheckbox();
+    createSearchForm();
+    createTable();
 
-//     document.querySelector("form").addEventListener("submit", e => {
-//         e.preventDefault();
-//         const searchString = document.getElementById("search").value.toLowerCase();
-//         addData(currentData.filter(robot => robot.name.toLowerCase().includes(searchString)));
-//     });
+    document.querySelector("form").addEventListener("submit", e => {
+        e.preventDefault();
+        const searchString = document.getElementById("search").value.toLowerCase();
+        addData(currentData.filter(robot => robot.name.toLowerCase().includes(searchString)));
+    });
 
-//     document.getElementById("isVip").addEventListener("change", e => {
-//         addData(e.target.checked ? currentData.filter(robot => robot.vip) : currentData);
-//     });
+    document.getElementById("isVip").addEventListener("change", e => {
+        addData(e.target.checked ? currentData.filter(robot => robot.vip) : currentData);
+    });
 
-//     async function fetchData() {
-//         try {
-//             let response = await fetch(url);
-//             currentData = await response.json();
-//             addData(currentData);
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
+    async function fetchData() {
+        try {
+            let response = await fetch(url);
+            currentData = await response.json();
+            addData(currentData);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-//     fetchData();
-// }
+    fetchData();
+}
--- pamoka40/uzduotis3-2.js
+++ pamoka40/uzduotis3-2.js
@@ -1,19 +1,19 @@
-// const form = document.getElementById("add-car-form");
-// const notification = document.getElementById("notification");
+const form = document.getElementById("add-car-form");
+const notification = document.getElementById("notification");
 
-// form.addEventListener("submit", event => {
-//     event.preventDefault();
+form.addEventListener("submit", event => {
+    event.preventDefault();
 
-//     let brand = document.getElementById("brand-input").value;
-//     let model = document.getElementById("model-input").value;
+    let brand = document.getElementById("brand-input").value;
+    let model = document.getElementById("model-input").value;
 
-//     fetch("https://olive-bead-glazer.glitch.me", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ brand: brand, model: model }),
-//     }).then(response => {
-//         if (response.ok) {
-//             notification.innerHTML = "Car added successfully!";
-//         }
-//     });
-// });
+    fetch("https://olive-bead-glazer.glitch.me", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ brand: brand, model: model }),
+    }).then(response => {
+        if (response.ok) {
+            notification.innerHTML = "Car added successfully!";
+        }
+    });
+});
