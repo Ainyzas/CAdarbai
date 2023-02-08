573e20fafc324804ab929e07d101a2bbd24aa689
ainas <puidokasainas@gmail.com>
2023-02-08 19:58:57 +0200

    accomplished additional task, to check cars object

---
+++ NodeJS/pamoka46/routes/userCarBrand/route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from '../../database/tasks2.js';
+const router = express.Router();
+
+router.get('/car/:make', (req, res) => {
+  const brand = req.params.make;
+
+  const result = data[brand];
+
+  res.json(result);
+});
+
+export default router;
