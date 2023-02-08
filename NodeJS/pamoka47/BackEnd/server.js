d500f90c17ea41076d63fe2f82ff6aeb57cd4aad
ainas <puidokasainas@gmail.com>
2023-02-08 18:53:16 +0200

    added in eslint

--- NodeJS/pamoka47/BackEnd/server.js
+++ NodeJS/pamoka47/BackEnd/server.js
@@ -1,8 +1,11 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes/mainRouter.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './routes/mainRouter.js';
 
-const PORT = 3000;
+dotenv.config();
+const { PORT } = process.env;
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +13,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log("app is running on port " + PORT);
+  console.log(`app is running on port ${PORT}`);
 });
