1ee1251d4b9914420905d81ea6251c20c54ca84b
ainas <puidokasainas@gmail.com>
2023-02-27 20:37:36 +0200

    finished task

--- NodeJS SQL/pamoka54/BackEnd/controllers/controller.js
+++ NodeJS SQL/pamoka54/BackEnd/controllers/controller.js
@@ -1,1 +1,34 @@
 import DB from '../db/db.js';
+
+export async function getItems(req, res) {
+  try {
+    const { limit } = req.query;
+    const items = await DB.query(`SELECT id, title FROM items ORDER BY id asc limit ${limit}`);
+
+    res.json(items.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export async function postItem(req, res) {
+  try {
+    const { title } = req.body;
+    const item = await DB.query(`INSERT INTO items (title) VALUES ('${title}')`);
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export async function deleteItem(req, res) {
+  try {
+    const { id } = req.params;
+    const item = await DB.query(`DELETE FROM items WHERE id = ${id}`);
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- NodeJS SQL/pamoka54/BackEnd/routes/router.js
+++ NodeJS SQL/pamoka54/BackEnd/routes/router.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import { getItems, postItem, deleteItem } from '../controllers/controller.js';
+import { checkLimit, checkBody, checkForId } from '../middleware/middleware.js';
 
 const router = express.Router();
+
+router.get('/items', checkLimit, getItems);
+
+router.post('/items', checkBody, postItem);
+
+router.delete('/items/:id', checkForId, deleteItem);
 
 export default router;
